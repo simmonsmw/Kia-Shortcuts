3ea56f2e4b31e23b3c8724e079d42574d954802e
simmonsmw <39995848+simmonsmw@users.noreply.github.com>
2025-04-04 11:00:40 -0500

    heating=1 enable

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def start_climate():
             set_temp=74,  # Set temperature in Fahrenheit
             duration=15,   # Duration in minutes
        #     defrost=False,
-        #    heating=1,
+            heating=1,
             steering_wheel=1,         # 1 = on, 0 = off
      #       front_left_seat=1         # 1 = low, 2 = medium, 3 = high (try 1 first)    
         )
