f275463deb5dce708d5b92a90ae88a2ada2da6f6
simmonsmw <39995848+simmonsmw@users.noreply.github.com>
2025-04-04 11:21:15 -0500

    removed seat heating as its not working at this time from KIA

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def start_climate():
        #     defrost=False,
             heating=1,
             steering_wheel=1,        # 1 = on, 0 = off
-           front_left_seat=8         # 1 = low, 2 = medium, 3 = high (try 1 first)    
+         #  front_left_seat=8        doesn't work at this time 4/4/25
         )
 
         # Start climate control using the VehicleManager's start_climate method
